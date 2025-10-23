3858c38f7b2460660151afbf27e6022bb4634bef
SixtenGedda <sixten.gedda@gmail.com>
2025-10-23 20:38:54 +0200

    Add acpi lid close/open functionality

--- clamshell.sh
+++ clamshell.sh
@@ -1,42 +1,29 @@
+#!/bin/bash
+
+#on boot -> exec-once
+
+#on wake -> systemd service
+
+#on plug -> acpid
+
+echo "$(date '+%H:%M:%S') ACPI triggered" >>/tmp/clamshell.log
+
+export XDG_RUNTIME_DIR="/run/user/1000"
+export HYPRLAND_INSTANCE_SIGNATURE=$(ls /run/user/1000/hypr | head -n 1)
+
 KBD_BACKLIGHT="/sys/class/leds/asus::kbd_backlight/brightness"
+LID_STATE=$(cat /proc/acpi/button/lid/*/state | awk '{print $2}')
 
-saved_brightness = $(cat $KBD_BACKLIGHT)
+echo "$LID_STATE"
 
+SAVED_BRIGHTNESS=$(cat $KBD_BACKLIGHT)
 
-suspend() {
-  systemctl suspend
+if [[ "$LID_STATE" == "closed" ]]; then
+  sleep 0.2
+  hyprctl keyword monitor "eDP-1, disable"
+  echo 0 | tee "$KBD_BACKLIGHT"
+else
+  echo 3 | tee $KBD_BACKLIGHT
+  hyprctl keyword monitor "eDP-1, preferred, auto, 1"
 
-}
-
-disable_kb_backlight () {
- echo 0 | sudo tee $KBD_BACKLIGHT > /dev/null
-
-}
-
-enable_kb_backlight () {
-  
-}
-
-enable_internal_display() {
-    hyprctl keyword monitor "eDP-1,preferred,auto,1"
-
-}
-
-# EVENT LISTENERS
-
-handle_lid_close_external() {
-  disable_kb_backlight()
-}
-
-handle_lid_close () {
-  suspend()
-}
-
-handle_lid_open() {
-  enable_internal_display()
-  
-}
-
-listen_acpi_events() {
-  
-}
+fi
